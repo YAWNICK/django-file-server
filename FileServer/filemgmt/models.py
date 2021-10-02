a6a06c49ab5bec2ae8a78ced94cf64bb78add563
YAWNICK <jaweiss1999@googlemail.com>
2021-10-02 10:29:56 +0200

    first version

---
+++ FileServer/filemgmt/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+
+
+def upload_path(instance, filename):
+    return settings.MEDIA_ROOT/filename
+
+
+class File(models.Model):
+
+    file = models.FileField(upload_to=upload_path)
+
+    def __str__(self) -> str:
+        return self.file.name.split('/')[-1]
